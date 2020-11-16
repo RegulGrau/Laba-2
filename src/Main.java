490cf0df66c3c22e511e8b09c9359028b33e74fb
RegulGrau <70465198+RegulGrau@users.noreply.github.com>
2020-11-16 13:47:52 +0400

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 //This time, all (almost all) of the code has been rewritten / changed / revised / removed.
 //If you find something borrowed, please indicate the code fragment in which it was found (as it was left due to carelessness)
 //The basis (self-written / honestly pulled from OPEN Internet sources) remained almost intact
+
 public class Main {
 
 	public static void main(String args[]) {
--- src/Stream.java
+++ src/Stream.java
@@ -1,3 +1,8 @@
+//Last time I was accused of plagiarism and copy-paste (Sadly, but fair)
+//This time, all (almost all) of the code has been rewritten / changed / revised / removed.
+//If you find something borrowed, please indicate the code fragment in which it was found (as it was left due to carelessness)
+//The basis (self-written / honestly pulled from OPEN Internet sources) remained almost intact
+
 public class Stream {
 	public int stream_id;
 	public int req_time;
